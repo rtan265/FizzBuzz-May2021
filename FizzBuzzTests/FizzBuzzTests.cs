42ead58209542283a56b75188565a58edeac662b
Ryan Tan <rtan265@aucklanduni.ac.nz>
2021-05-12 15:47:45 +1200

    Added failing tests

--- FizzBuzzTests/FizzBuzzTests.cs
+++ FizzBuzzTests/FizzBuzzTests.cs
@@ -12,9 +12,41 @@ namespace FizzBuzzTests
         [InlineData(12)]
         [InlineData(18)]
         [InlineData(21)]
+        public void PrintFizz(int value)
+        {
+            string expected = "Fizz";
+
+            var actual = "";
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData(5)]
+        [InlineData(10)]
+        [InlineData(20)]
+        [InlineData(25)]
+        [InlineData(35)]
+        [InlineData(40)]
         public void PrintBuzz(int value)
         {
-            string expected = "FIZZ";
+            string expected = "Buzz";
+
+            var actual = "";
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData(15)]
+        [InlineData(30)]
+        [InlineData(45)]
+        [InlineData(60)]
+        [InlineData(75)]
+        [InlineData(90)]
+        public void PrintFizzBuzz(int value)
+        {
+            string expected = "FizzBuzz";
 
             var actual = "";
 
