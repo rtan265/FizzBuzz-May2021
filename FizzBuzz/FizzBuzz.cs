c7a05085eb2e0ec5dd93bb1df801721e2fd56ee0
Ryan Tan <rtan265@aucklanduni.ac.nz>
2021-05-12 16:32:02 +1200

    Added FizzBuzz class for looping through numbers

---
+++ FizzBuzz/FizzBuzz.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    class FizzBuzz
+    {
+        public NumberCheckingService _numberCheckingService;
+
+        public FizzBuzz()
+        {
+            _numberCheckingService = new NumberCheckingService();
+        }
+
+        public void StartFizzBuzz()
+        {
+            for (int i = 1; i < 101; i++)
+            {
+                Console.WriteLine(_numberCheckingService.CheckFizzBuzz(i));
+            }
+        }
+    }
+}
